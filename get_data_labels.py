463a5a91e2d47973523fde86ebddb94b3f54a7c6
Jusill <Jusill98@gmail.com>
2020-03-12 22:01:56 +0300

    first commit

---
+++ get_data_labels.py
@@ -0,0 +1,31 @@
+import os
+from shutil import copytree
+
+STOP_SIGN = "Stop Sign"
+SWIPING_LEFT = "Swiping Left"
+SWIPING_RIGHT = "Swiping Right"
+DIR = "./20bn-jester-v1/"
+NEW_DIR = "./new_lables/"
+
+lables = set()
+file = open("lables.csv", "r")
+for line in file:
+    lables.add(line.split("\t")[0])
+
+lables = list(lables)
+print(len(lables))
+
+'''
+lables_df = pd.read_csv("lables.csv", sep="\t", headers=None)
+lables = set(list(lables_df[1]))
+'''
+
+i = 0
+for name_dir in os.listdir(DIR):
+    if name_dir in lables:
+        i = i + 1
+        if i % 100 == 0:
+             print(i)
+
+        copytree(DIR + name_dir, NEW_DIR + name_dir)
+
---
+++ proccesing.py
@@ -0,0 +1,41 @@
+import csv
+import cv2
+import numpy as np
+import tensorflow as tf
+import os
+from hand_tracker import HandTracker
+
+WINDOW = "Hand Tracking"
+PALM_MODEL_PATH = "./palm_detection_without_custom_op.tflite"
+LANDMARK_MODEL_PATH = "./hand_landmark.tflite"
+#LANDMARK_MODEL_PATH = "./hand_landmark_3d.tflite"
+ANCHORS_PATH = "./anchors.csv"
+PATH = "./new_lables/"
+
+detector = HandTracker(
+    PALM_MODEL_PATH,
+    LANDMARK_MODEL_PATH,
+    ANCHORS_PATH,
+    box_shift=0.2,
+    box_enlarge=1.3
+)
+
+lables = {}
+lables_file = open("lables.csv", "w")
+for line in lables:
+    lables[line.split("\t")[0]] = line.split("\t")[1]
+
+output_file = open("output.txt", "w")
+for name_dir in os.listdir(PATH):
+
+    output_file.write(name_dir + "\t" + lables[name_dir] + "\t" + str(len(os.listdir(PATH + name_dir))) + "\t")
+
+    for name_file in os.listdir(PATH + name_dir):
+        image = cv2.imread(PATH + name_dir + "/" + name_file, flags=cv2.IMREAD_COLOR)
+        points, bbox = detector(image)
+
+        if points is not None:
+            for p in points:
+                output_file.write(str(p[0]) + " " + str(p[1]) + " ")
+
+    output_file.write("\n")
