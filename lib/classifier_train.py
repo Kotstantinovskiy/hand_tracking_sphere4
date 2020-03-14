b84298e75667febbbe248a07755e834b158fab4c
miroslav.morozov <miroslav.morozov@corp.mail>
2020-03-14 17:14:06 +0300

    Move and pretty

---
+++ lib/classifier_train.py
@@ -0,0 +1,15 @@
+from catboost import CatBoostRegressor
+from uril
+import numpy as np
+
+class Classifier:
+    def __init__(self):
+        self.model = CatBoostRegressor()
+
+    def train(self, data):
+        X = np.empty((len(data), 32 * 42))
+        for i, gest in enumerate(data):
+            if len(gest) < 17:
+                continue
+            if len(gest) < 32:
+                gest = pretty_gest(gest, 32)
