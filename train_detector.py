ae4885955faaf9f0ce6c27b18d875502c4330c99
Jusill <Jusill98@gmail.com>
2020-03-14 17:00:42 +0300

    add len

--- train_detector.py
+++ train_detector.py
@@ -1,30 +1,30 @@
 from catboost import CatBoostRegressor
 import numpy as np
+from ./lib/Gesture import Gesture
+
+class Detector:
+
+    def __init__(self, iterations=800, lr=0.1, depth=8):
+        detector = CatBoostRegressor(iterations=iterations,
+                                     learning_rate=lr,
+                                     depth=depth)
+
+    def train(self, gestures, window=8):
+
+        X_train = list()
+        y_list = list()
+
+        for gesture in gestures:
+
+            for g in gesture.slice(i, i+window):
 
 
-def train_detector(path_train, window=8, iterations=800, learning_rate=0.1, depth=8):
-
-    detector = CatBoostRegressor(iterations=iterations,
-                                 learning_rate=learning_rate,
-                                 depth=depth)
-
-    X_train = list()
-    y_train_class = list()
+        with open(path_train, "r") as train_file:
+            for line in train_file:
+                parts = line.split("\t").strip()
 
 
-    with open(path_train, "r") as train_file:
-        for line in train_file:
-            parts = line.split("\t").strip()
-
-        name_dir = int(parts[0])
-        lable = parts[1]
-        len_ = int(parts[2])
-        vecs = np.array(list(map(float, parts[3].split(" "))))
-
-        index_sliding = np.arange(window*63)[None, :] + 63*np.arange(int(vecs/63)-window)[:, None] ## ?????
-
-        vecs = vecs[index_sliding].reshape((-1, 63, -1))
-        X_train.append(vecs)
-        y_train_class.append(lable)
 
 
+
+    def predict(self):
